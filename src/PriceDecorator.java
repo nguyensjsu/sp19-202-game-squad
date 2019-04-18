e83646389a6fccb0ae8e73ddd3b9d4dfd8b86adc
Megha <megha@sjsu.edu>
2019-04-17 21:58:54 -0700

    Implement price decorator

---
+++ src/PriceDecorator.java
@@ -0,0 +1,35 @@
+/**
+ * Write a description of class PowerDecorator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import greenfoot.*;
+public class PowerDecorator implements KeyListener 
+{
+   
+    KeyListener keyListener;
+    
+    public PowerDecorator(KeyListener keyListener)
+    {   
+        this.keyListener = keyListener;
+    }
+
+    public void keyLeftAction() {
+        keyListener.keyLeftAction();
+    }
+    
+     public void keyRightAction() {
+         keyListener.keyRightAction();
+    }
+    
+     public void keyUpAction() {
+        keyListener.keyUpAction();
+    }
+    
+     public void keyDownAction() {
+       keyListener.keyDownAction();
+    }
+}
+
