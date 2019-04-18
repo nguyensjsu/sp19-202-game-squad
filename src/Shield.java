ee8c94411da4711fde1595df6ad08df9069afa7f
anjali.deshmukh <anjali.deshmukh@pubmatic.com>
2019-04-18 15:07:07 -0700

    Shield object

---
+++ src/Shield.java
@@ -0,0 +1,34 @@
+
+
+
+import greenfoot.*; 
+/**
+ * Write a description of class Shield here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Shield extends Actor 
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Shield
+     */
+    public Shield()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
