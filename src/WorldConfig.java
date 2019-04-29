996064fa395ee81915229be6a703fd72afe199e1
Chirag Arora <chiragkarora@gmail.com>
2019-04-29 03:06:41 -0700

    code clean up, configs cleanup

--- src/WorldConfig.java
+++ src/WorldConfig.java
@@ -5,38 +5,43 @@
  * @version (a version number or a date)
  */
 public class WorldConfig {
-   
+
     private static WorldConfig worldConfig;
     public DifficultyStrategy ds;
 
     private static int x = 800;
     private static int y = 800;
     private static int cellSize = 1;
+    
+    // Turtle configuration
     public int TURTLE_SPEED = 2;
     public int TURTLE_DEGREE = 3;
+    
+    // Ammo configuration
     public int SHOTS = 10;
 
+    // Level  configuration
     public int MAX_LEVEL=3;
     public int LEVEL_FACTOR=20;
 
+    // Lettuce configuration
     public int NUM_OF_LETTUCE = 10;
     public int RED_LETTUCE_CREATION_ON_SCORE = 20;
     public int NUM_OF_RED_LETTUCE = 1;
     public int LETTUCE_CREATION_DELAY = 30;
 
-    // these will come from strategy pattern
+    // Snake configuration
+    // these will come from strategy pattern, defaults
     public int NUM_OF_SNAKES = 10;
     public int SPEED_OF_SNAKES = 1;
     public int SNAKES_ATTRACTION = 50;
-    //==================
-
     public int SNAKE_CREATION_DELAY = 30;
 
+    // Bugs configuration
     public int NUM_OF_BUGS = 1;
     public int BUG_SPEED = 1;
     public int BUG_CREATION_DELAY = 60;
 
-    
     private WorldConfig(DifficultyStrategy ds)
     {
         this.ds = ds;
