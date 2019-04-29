b50f23e95cbbdcd4b09fd10f57979c6b63d023d8
Chirag Arora <chiragkarora@gmail.com>
2019-04-29 02:46:14 -0700

    shot fix, so we don't have shots lieing at the world edge

--- src/Shot.java
+++ src/Shot.java
@@ -16,19 +16,38 @@ public class Shot extends Actor
     {
         move(25);
         if (canSee(Snake.class)) {
-            Actor actor = getOneObjectAtOffset(0, 0, Snake.class);
-            if(actor != null) {
-                getWorld().removeObject(actor);
+            Actor snake = getOneObjectAtOffset(0, 0, Snake.class);
+            if(snake != null) {
+                Snake sn = (Snake)snake;
+                getWorld().removeObject(snake);
+                getWorld().removeObject(this);
+            }
+        } else {
+            if (atWorldEdge()) {
                 getWorld().removeObject(this);
             }
         }
+        
+        
     }
 
-    public boolean canSee(Class clss)
+    private boolean canSee(Class clss)
     {
         Actor actor = getOneObjectAtOffset(0, 0, clss);
         return actor != null;        
     }
 
+    /**
+     * Test if we are close to one of the edges of the world. Return true if we are.
+     */
+    private boolean atWorldEdge()
+    {
+        if(getX() < 10 || getX() > getWorld().getWidth() - 10)
+            return true;
+        if(getY() < 10 || getY() > getWorld().getHeight() - 10)
+            return true;
+        else
+            return false;
+    }
 }
 
